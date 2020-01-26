a95886b8aef33686435d3157cb62fa7038dcbf42
Alisson Medeiros <alisson.medeiros@gmail.com>
2020-01-26 20:14:06 -0300

    Correção de plugin

--- src/test/java/org/jornadacolaborativa/CalculadoraTest.java
+++ src/test/java/org/jornadacolaborativa/CalculadoraTest.java
@@ -1,12 +1,13 @@
 package org.jornadacolaborativa;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class CalculadoraTest {
 
     @Test
     public void teste() {
-
+        Assertions.fail("");
     }
 
 }
